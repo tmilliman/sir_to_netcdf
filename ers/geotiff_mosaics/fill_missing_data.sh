9862565d2deddd74dd08f4b52b0f08e5e399d893
Thomas Milliman <thomas.milliman@unh.edu>
2022-11-14 11:10:45 -0500

    add file to add months of missing data

---
+++ ers/geotiff_mosaics/fill_missing_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# script to copy empty geotiff to fill in missing months
+cp ers_missing_data_month.tif ers_ers1_mean_db_1995_04.tif
+cp ers_missing_data_month.tif ers_ers1_std_db_1995_04.tif
+cp ers_missing_data_month.tif ers_ers1_mean_db_1996_05.tif
+cp ers_missing_data_month.tif ers_ers1_std_db_1996_05.tif
